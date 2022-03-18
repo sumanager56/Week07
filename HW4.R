f6dec6450d476f7933ed0d6bfba0547f8292c624
sumanager56 <99036349+sumanager56@users.noreply.github.com>
2022-03-17 20:37:51 -0400

    Create HW4.R

---
+++ HW4.R
@@ -0,0 +1,250 @@
+objects()
+rm(list=objects())
+setwd("~/Week07")
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(EcoHydRology,curl,httr,rnoaa,raster,shapefiles,rgdal,elevatr)
+
+#StremLab @37.202529,-80.4713048
+
+lon=-80.4713048
+lat=37.202529
+trunc((180+lon)/6+1)
+proj4_utm = paste0("+proj=utm +zone=", trunc((180+lon)/6+1), " +datum=WGS84 +units=m +no_defs")
+print(proj4_utm)
+
+
+proj4_ll = "+proj=longlat"
+
+crs_ll=CRS(proj4_ll)
+crs_utm=CRS(proj4_utm)
+print(crs_ll)
+print(crs_utm)
+
+#myflowgage$area   # area in km2
+latlon <- cbind(lon,lat)
+gagepoint_ll <- SpatialPoints(latlon)
+proj4string(gagepoint_ll)=proj4_ll
+gagepoint_utm=spTransform(gagepoint_ll,crs_utm)
+# Open up maps.google.com to guesstimate area/lengths
+url=paste0("https://www.google.com/maps/@",
+           "lat","lon","18z")
+browseURL(url)
+
+#Area=31 km2
+searchlength=sqrt(31*8)*1000 
+pourpoint=SpatialPoints(gagepoint_utm@coords,proj4string = crs_utm)
+bboxpts=gagepoint_utm@coords
+bboxpts=rbind(bboxpts,bboxpts+searchlength)
+bboxpts=rbind(bboxpts,bboxpts-searchlength)
+bboxpts
+bboxpts=rbind(bboxpts,c(min(bboxpts[,1]),max(bboxpts[,2])))
+bboxpts=rbind(bboxpts,c(max(bboxpts[,1]),min(bboxpts[,2])))
+bboxpts
+bboxpts=SpatialPoints(bboxpts,proj4string = crs_utm)
+
+# From Lab04, get your DEM
+mydem=get_aws_terrain(locations=bboxpts@coords, 
+                      z = 12, prj = proj4_utm,src ="aws",expand=1)
+dev.off()
+res(mydem)
+plot(mydem)
+plot(bboxpts,add=T)
+plot(pourpoint,add=T,col="red")
+
+old_path <- Sys.getenv("PATH")
+old_path
+Sys.setenv(PATH = paste(old_path,
+                        paste0(Sys.getenv("HOME"),"/TauDEM/bin"), 
+                        sep = ":"))
+system("mpirun aread8")
+
+writeRaster(mydem,filename = "mydem.tif",overwrite=T)
+
+z=raster("mydem.tif")
+plot(z)
+
+# Pitremove
+system("mpiexec -n 8 pitremove -z mydem.tif -fel mydemfel.tif")
+fel=raster("mydemfel.tif")
+plot(fel)
+
+plot(z-fel)
+# D8 flow directions
+system("mpiexec -n 8 d8flowdir -p mydemp.tif -sd8 mydemsd8.tif -fel mydemfel.tif",show.output.on.console=F,invisible=F)
+p=raster("mydemp.tif")
+plot(p)
+sd8=raster("mydemsd8.tif")
+plot(sd8)
+
+# Contributing area
+system("mpiexec -n 8 aread8 -p mydemp.tif -ad8 mydemad8.tif")
+ad8=raster("mydemad8.tif")
+plot(log(ad8))
+#zoom(log(ad8))
+
+
+# Grid Network 
+system("mpiexec -n 8 gridnet -p mydemp.tif -gord mydemgord.tif -plen mydemplen.tif -tlen mydemtlen.tif")
+gord=raster("mydemgord.tif")
+plot(gord)
+#zoom(gord)
+
+# DInf flow directions
+system("mpiexec -n 8 dinfflowdir -ang mydemang.tif -slp mydemslp.tif -fel mydemfel.tif",show.output.on.console=F,invisible=F)
+ang=raster("mydemang.tif")
+plot(ang)
+slp=raster("mydemslp.tif")
+plot(slp)
+
+
+# Dinf contributing area
+system("mpiexec -n 8 areadinf -ang mydemang.tif -sca mydemsca.tif")
+sca=raster("mydemsca.tif")
+plot(log(sca))
+#zoom(log(sca))
+
+# Threshold
+system("mpiexec -n 8 threshold -ssa mydemad8.tif -src mydemsrc.tif -thresh 2000")
+src=raster("mydemsrc.tif")
+plot(src)
+zoom(src)
+plot(pourpoint,add=T)
+
+# a quick R function to write a shapefile  #how much water is going
+makeshape.r=function(sname="shape",n=1)
+{
+  xy=locator(n=n)
+  points(xy)
+  
+  #Point
+  dd <- data.frame(Id=1:n,X=xy$x,Y=xy$y)
+  ddTable <- data.frame(Id=c(1),Name=paste("Outlet",1:n,sep=""))
+  ddShapefile <- convert.to.shapefile(dd, ddTable, "Id", 1)
+  write.shapefile(ddShapefile, sname, arcgis=T)
+}
+
+makeshape.r("ApproxOutlets")
+
+# Move Outlets
+system("mpiexec -n 8 moveoutletstostrm -p mydemp.tif -src mydemsrc.tif -o ApproxOutlets.shp -om Outlet.shp")
+outpt=read.shp("Outlet.shp")
+approxpt=read.shp("ApproxOutlets.shp")
+
+plot(src)
+points(outpt$shp[2],outpt$shp[3],pch=19,col=2)
+points(approxpt$shp[2],approxpt$shp[3],pch=19,col=4)
+
+zoom(src)
+# Contributing area upstream of outlet
+system("mpiexec -n 8 aread8 -p mydemp.tif -o Outlet.shp -ad8 mydemssa.tif")
+ssa=raster("mydemssa.tif")
+plot(ssa) 
+
+
+# Threshold
+system("mpiexec -n 8 threshold -ssa mydemssa.tif -src mydemsrc1.tif -thresh 4000")
+src1=raster("mydemsrc1.tif")
+plot(src1)
+zoom(src1)
+
+# Stream Reach and Watershed
+system("mpiexec -n 8 streamnet -fel mydemfel.tif -p mydemp.tif -ad8 mydemad8.tif -src mydemsrc1.tif -o Outlet.shp -ord mydemord.tif -tree mydemtree.txt -coord mydemcoord.txt -net mydemnet.shp -w mydemw.tif")
+plot(raster("mydemord.tif"),add=T)
+#zoom(raster("mydemord.tif"))
+plot(raster("mydemw.tif"),add=T)
+
+# Plot streams using stream order as width
+snet=read.shapefile("mydemnet")
+ns=length(snet$shp$shp)
+for(i in 1:ns)
+{
+  lines(snet$shp$shp[[i]]$points,lwd=snet$dbf$dbf$Order[i])
+}
+
+# Peuker Douglas stream definition
+system("mpiexec -n 8 peukerdouglas -fel mydemfel.tif -ss mydemss.tif")
+ss=raster("mydemss.tif")
+plot(ss)
+zoom(ss)
+
+#  Accumulating candidate stream source cells
+system("mpiexec -n 8 aread8 -p mydemp.tif -o Outlet.shp -ad8 mydemssa.tif -wg mydemss.tif")
+ssa=raster("mydemssa.tif")
+plot(ssa)
+
+#  Drop Analysis
+system("mpiexec -n 8 dropanalysis -p mydemp.tif -fel mydemfel.tif -ad8 mydemad8.tif -ssa mydemssa.tif -drp mydemdrp.txt -o Outlet.shp -par 5 500 10 0")
+
+# Deduce that the optimal threshold is 300 
+# Stream raster by threshold
+system("mpiexec -n 8 threshold -ssa mydemssa.tif -src mydemsrc2.tif -thresh 4000")
+plot(raster("mydemsrc2.tif"))
+
+# Stream network
+system("mpiexec -n 8 streamnet -fel mydemfel.tif -p mydemp.tif -ad8 mydemad8.tif -src mydemsrc2.tif -ord mydemord2.tif -tree mydemtree2.dat -coord mydemcoord2.dat -net mydemnet2.shp -w mydemw2.tif -o Outlet.shp",show.output.on.console=F,invisible=F)
+
+plot(raster("mydemw2.tif"))
+zoom(raster("mydemw2.tif"))
+snet=read.shapefile("mydemnet2")
+ns=length(snet$shp$shp)
+for(i in 1:ns)
+{
+  lines(snet$shp$shp[[i]]$points,lwd=snet$dbf$dbf$Order[i])
+}
+
+# Wetness Index
+system("mpiexec -n 8 slopearearatio -slp mydemslp.tif -sca mydemsca.tif -sar mydemsar.tif", show.output.on.console=F, invisible=F)
+sar=raster("mydemsar.tif")
+wi=sar
+wi[,]=-log(sar[,])
+plot(wi)
+
+# Distance Down
+system("mpiexec -n 8 dinfdistdown -ang mydemang.tif -fel mydemfel.tif -src mydemsrc2.tif -m ave v -dd mydemdd.tif",show.output.on.console=F,invisible=F)
+plot(raster("mydemdd.tif"))
+
+# Trimming, Cropping, and Masking to make life prettier and easier
+
+mydemw=raster("mydemw.tif")
+mybasinmask=trim(mydemw,padding=2)
+mybasindem=crop(mydem,mybasinmask)
+mybasindem=mask(mybasindem,mybasinmask)
+plot(mybasindem)
+
+# Wetness Index
+mybasinslp=crop(slp,mybasinmask)
+mybasinslp=mask(mybasinslp,mybasinmask)
+plot(mybasinslp)
+
+mybasinsca=crop(sca,mybasinmask)
+mybasinsca=mask(mybasinsca,mybasinmask)
+plot(mybasinsca)
+
+TI = log( (mybasinsca+1)/(mybasinslp+0.00001) )
+plot(TI)
+zoom(TI)
+
+pacman::p_load(classInt)
+nTIclass=5 #number of TI classes, currently equal area, can adjust method various ways e.g., classIntervals(v, n = nTIclass, style = "jenks")
+v=values(TI)
+v=v[!is.na(v)]
+brks.qt = classIntervals(v, n = nTIclass, style = "quantile")$brks #length nTIclass+1 of just the numeric breakpoints
+TIC = cut(TI, breaks=brks.qt, include.lowest = T, right=T)
+#
+# A series of plots to show all of the components
+#For difference between DEM and filled DEM
+plot(z-fel)
+mybasindif=crop(fel,mybasinmask)
+mybasindif=mask(mybasindif,mybasinmask)
+plot(mybasindif-mybasindem)
+
+#plotting all together
+par(mfrow = c(2, 2))
+plot(TI)
+plot(TIC)
+plot(mybasindem)
+plot(mybasindif-mybasindem)
+#plot(mybasinsca)
+#plot(mybasinslp)
+
+dev.off()
